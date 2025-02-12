fae641e238d157a497364cfb51ecf88512344922
SadmanSalim <sadmanmd225@gmail.com>
2025-02-13 01:37:07 +0600

    database

--- database/tanvir_protfolio.sql
+++ database/tanvir_protfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2025 at 02:25 PM
+-- Generation Time: Feb 12, 2025 at 08:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `tanvir_protfolio`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `id` int(11) NOT NULL,
+  `aboutme` varchar(455) DEFAULT NULL,
+  `img` varchar(11) DEFAULT NULL,
+  `stitle` varchar(255) DEFAULT NULL,
+  `sdesc` varchar(455) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `age` varchar(11) DEFAULT NULL,
+  `website` varchar(25) DEFAULT NULL,
+  `degree` varchar(25) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `freelance` varchar(255) DEFAULT NULL,
+  `bdesc` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `aboutme`, `img`, `stitle`, `sdesc`, `birth_date`, `age`, `website`, `degree`, `phone`, `email`, `city`, `freelance`, `bdesc`) VALUES
+(1, 'Md jUman', NULL, 'Veniam in quidem co', 'Qui voluptates lorem', '2010-10-18', 'Mollitia nu', 'https://www.gej.com.au', 'Sint quia iure mini', '61', 'sebex@mailinator.com', 'Sapiente doloremque ', 'freelance', 'Explicabo Ut facere');
 
 -- --------------------------------------------------------
 
@@ -42,9 +72,34 @@ INSERT INTO `admin` (`id`, `fname`, `email`, `password`) VALUES
 (1, 'Saalim Sadman', 'sadmanmd225@gmail.com', '$$2y$10$nMTz6.O.3S50dQdil1TrrOB5mFqmS.nt4aGa7kQ7JSlpy2YDnZ3jK'),
 (2, 'Juman', 'juman@gmail.com', '$$2y$10$uDHxOXmNEpa7pT/VB0pXOu2eDB/uSY8v3rCaq0pxfR2uK5C56K3c.');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner`
+--
+
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `skills` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `banner`
+--
+
+INSERT INTO `banner` (`id`, `name`, `skills`) VALUES
+(1, 'Tanvir Shawon', 'Digital Marketing, Developer');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `admin`
@@ -53,13 +108,31 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `banner`
+--
+ALTER TABLE `banner`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
